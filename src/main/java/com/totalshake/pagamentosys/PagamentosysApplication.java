08e896d672782b87ea4928e410d1799a6b8812bd
amostaro <amostaro@ciandt.com>
2022-08-04 16:30:54 -0300

    initial commit

---
+++ src/main/java/com/totalshake/pagamentosys/PagamentosysApplication.java
@@ -0,0 +1,13 @@
+package com.totalshake.pagamentosys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PagamentosysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PagamentosysApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/totalshake/pagamentosys/PagamentosysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.totalshake.pagamentosys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PagamentosysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
